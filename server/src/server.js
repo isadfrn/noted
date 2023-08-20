6f1d093200295f65acb0590b8b1192d212069aeb
isadfrn <isadfrn@gmail.com>
2023-08-20 18:45:47 -0300

    feat: Add router logic

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const showBanner = require("node-banner");
+const routes = require("./routes");
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+
 const port = process.env.PORT || 3333;
 
 app.listen(port, async () => {
