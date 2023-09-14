7c6a330e57ccbe17cc130e7811ce7266d13ba8df
isadfrn <isadfrn@gmail.com>
2023-09-14 20:29:06 -0300

    feat: Add more notes filters

--- server/src/controllers/NotesController.js
+++ server/src/controllers/NotesController.js
@@ -58,9 +58,26 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id } = request.query;
+    const { user_id, title, tags } = request.query;
 
-    const notes = await knex("notes").where({ user_id }).orderBy("title");
+    let notes;
+
+    if (tags) {
+      const filterTags = tags.split(",");
+
+      notes = await knex("tags")
+        .select(["notes.id", "notes.title", "notes.user_id"])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title");
+    } else {
+      notes = await knex("notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+    }
 
     return response.status(200).json(notes);
   }
