4150717d2456bdd2ec63bd3e6c6e98c25fb5e754
isadfrn <isadfrn@gmail.com>
2023-11-16 17:31:39 -0300

    feat: Add Profile page

---
+++ client/src/pages/Profile/index.jsx
@@ -0,0 +1,41 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Form, Avatar } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <Link to="/">
+          <FiArrowLeft />
+        </Link>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img
+            src="https://github.com/isadfrn.png"
+            alt="A white girl using a blue shirt, with short hair, using glasses, in a green background"
+          />
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+
+        <Input placeholder="Name" type="text" icon={FiUser} />
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+
+        <Input placeholder="Current password" type="password" icon={FiLock} />
+
+        <Input placeholder="New password" type="password" icon={FiLock} />
+
+        <Button title="Save" />
+      </Form>
+    </Container>
+  );
+}
