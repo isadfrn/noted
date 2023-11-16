e18caf29afcc017e6d97464384fd5064d2615b2f
isadfrn <isadfrn@gmail.com>
2023-11-16 17:28:22 -0300

    feat: Add Button component

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -1,5 +1,9 @@
 import { Container } from "./styles";
 
-export function Button() {
-  return <Container type="button">Meu Botão</Container>;
-}
+export const Button = ({ title, loading = false, ...rest }) => {
+  return (
+    <Container type="button" disabled={loading} {...rest}>
+      {loading ? "Loading..." : title}
+    </Container>
+  );
+};
