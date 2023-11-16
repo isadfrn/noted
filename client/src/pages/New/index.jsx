98fe994443f8fa38f4a5696cb1e8ab674f581d26
isadfrn <isadfrn@gmail.com>
2023-11-16 17:31:21 -0300

    feat: Add New page

---
+++ client/src/pages/New/index.jsx
@@ -0,0 +1,42 @@
+import { Container, Form } from "./styles";
+import { Link } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Textarea } from "../../components/Textarea";
+import { Section } from "../../components/Section";
+import { NoteItem } from "../../components/NoteItem";
+import { Button } from "../../components/Button";
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Create note</h1>
+            <Link to="/">back</Link>
+          </header>
+
+          <Input placeholder="Title" />
+
+          <Textarea placeholder="More info" />
+
+          <Section title="Links">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="New link" />
+          </Section>
+
+          <Section title="Tags">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="New tag" />
+            </div>
+          </Section>
+
+          <Button title="Save" />
+        </Form>
+      </main>
+    </Container>
+  );
+}
