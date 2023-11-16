081d1ba9fe5f31927efda7b2ddd779f203fe7cb1
isadfrn <isadfrn@gmail.com>
2023-11-16 17:27:58 -0300

    feat: Add routing system

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
+import theme from "./styles/theme";
 import GlobalStyles from "./styles/global";
 
-import theme from "./styles/theme";
-
 import { ThemeProvider } from "styled-components";
-import { Details } from "./pages/Details";
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 );
