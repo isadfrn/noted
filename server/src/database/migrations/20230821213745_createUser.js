165683d133844fc2c0e6e4b58c22ce8c85cf63ce
isadfrn <isadfrn@gmail.com>
2023-11-23 10:45:09 -0300

    feat: Add upload configs

--- server/src/database/migrations/20230821213745_createUser.js
+++ server/src/database/migrations/20230821213745_createUser.js
@@ -5,7 +5,7 @@ exports.up = (knex) =>
     table.text("name").notNullable();
     table.text("email").unique().notNullable();
     table.text("password").notNullable();
-    table.text("avatar").notNullable();
+    table.text("avatar");
 
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,20 @@
 require("express-async-errors");
+require("dotenv").config();
 
 const AppError = require("./utils/AppError");
 const express = require("express");
 const showBanner = require("node-banner");
 const routes = require("./routes");
+const uploadConfig = require("./configs/upload");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
