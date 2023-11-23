2923ca1e9687068f13183d6990d5d1907fe91bd8
isadfrn <isadfrn@gmail.com>
2023-11-23 10:43:28 -0300

    feat: Add dinamic routes

--- server/src/routes/sessions.routes.js
+++ server/src/routes/sessions.routes.js
@@ -1,5 +1,6 @@
-const { Router } = require("express");
 const SessionsController = require("../controllers/SessionsController");
+
+const { Router } = require("express");
 
 const sessionsController = new SessionsController();
 const sessionsRoutes = Router();
